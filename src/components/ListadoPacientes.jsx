e228136ed690e048edcce7c98f66619739c605ba
Jesus Rojas <jarojas6524@misena.edu.co>
2022-03-17 04:03:14 -0500

    layout listado success

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,7 +1,23 @@
+import Paciente from "./Paciente"
+
 const ListadoPacientes = () => {
   return (
-    <div className="md:w-1/2 lg:w-3/5">
-      <h2>ListadoPacientes</h2>
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-center text-3xl"> Listado Pacientes </h2>
+      <p className="text-xl text-center mt-5 mb-10">
+        Administra tus {''}
+        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+      </p>
+      <Paciente />
+      <Paciente />
+      <Paciente />
+      <Paciente />
+      <Paciente />
+      <Paciente />
+      <Paciente />
+      <Paciente />
+      <Paciente />
+      <Paciente />
     </div>
   )
 }
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,23 @@
+const Paciente = () => {
+  return (
+    <div className="m-3 bg-white px-5 py-10 shadow-md rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+        <span className="font-normal normal-case">Hook</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+        <span className="font-normal normal-case">Jesus</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Correo: {''}
+        <span className="font-normal normal-case">asd@asd.asd</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {''}
+        <span className="font-normal normal-case">12-12-2020</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {''}
+        <span className="font-normal normal-case">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam, reiciendis? Voluptatum a est, placeat doloremque aliquid cupiditate labore animi, veritatis consequatur aut tenetur inventore deleniti sed quasi dolor quam aperiam?</span>
+      </p>
+    </div>
+  )
+}
+
+export default Paciente
